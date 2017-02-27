5dcd6eeaceda7dbef2a86f6699967de38597b23b
Jan Rybka <janry@netwise.pl>
2017-02-27 14:42:04 +0100

    pierwsze zmiany.

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>Angular2: Getting Started {{pageTitleAppendix}}</h1>
     `
 })
-export class AppComponent { }
+export class AppComponent { 
+    pageTitleAppendix: string = "Test";
+}
