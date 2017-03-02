9467be6c91c291b0635b910faf835a068caacfe7
Jan Rybka <jan.rybka@r4system.net>
2017-03-02 20:13:46 +0100

    Retrive over HTTP

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service'
 
 @Component({
     selector: 'pm-app',
     template: `
         <h1>Angular2: Getting Started {{pageTitleAppendix}}</h1>
         <pm-products></pm-products>
-    `
+    `,
+    providers: [ ProductService ]
 })
 export class AppComponent { 
     pageTitleAppendix: string = "Test";
